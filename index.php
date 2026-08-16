2bf42a695eb93f5878f8327671ab1ca59387176b
hemant-harry <sainihemant289@gmail.com>
2026-08-16 09:24:01 +0530

    Update index.php

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
     <h2>PHP is Working Successfully!</h2>
 
     <?php
-       echo "<p>Yesssssssss</p>";
+       echo "<p>This change is done in Github ! </p>";
         echo "<p>Hello from PHP</p>";
         echo "<p>Server Hostname: " . gethostname() . "</p>";
         echo "<p>PHP Version: " . phpversion() . "</p>";
